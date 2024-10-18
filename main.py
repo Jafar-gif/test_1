e9c9551205f6a455c68ad3b631759f7ca78e1d21
Jafar-gif <jafarortukov97@gmail.com>
2024-10-18 23:06:21 +0300

    второй комит_Способы вызова функции

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-def get_matrix(n,m,value):
-    matrix = []
 
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
 
-    return matrix
+def send_email(message,recipient,sender = 'ortukov97@inbox.ru'):
+    if '@' not in recipient or not recipient.endswith(('.com', '.ru', '.net')):
+        if'@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+        elif sender == recipient:
+            print('Нельзя отправить письмо самому себе!')
+        elif sender == 'ortukov97@inbox.ru':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+        elif sender != 'ortukov97@inbox.ru':
+            print (f'Нестандартный отправитель! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
-result1 = get_matrix(2,2,10)
-result2 = get_matrix(3,5,42)
-result3 = get_matrix(4,2,13)
-print(result1)
-print(result2)
-print(result3)
+send_email('message','stiw.box@mail.ru')
+send_email('message','recipient',)
+
+send_email('mesaage',{'stiw.box@mail.ru'},{'stiw.xod@mail.ru'})
+send_email('message','recipient','sender')
+
+send_email('message',{'ortukov97@inbox.ru'},{'ortukov97@inbox.ru'})
+send_email('message',{'sender'},{'sender'})
+
+
 
 
 
