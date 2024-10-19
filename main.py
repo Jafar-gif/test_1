4ce80dcd8bb509af7ba3f625ed0d5c4610738a72
Jafar-gif <jafarortukov97@gmail.com>
2024-10-19 15:50:38 +0300

    третий комит_Способы Рекурсия

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 
-
-def send_email(message,recipient,sender = 'ortukov97@inbox.ru'):
-    if '@' not in recipient or not recipient.endswith(('.com', '.ru', '.net')):
-        if'@' not in sender or not sender.endswith(('.com', '.ru', '.net')):
-            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-        elif sender == recipient:
-            print('Нельзя отправить письмо самому себе!')
-        elif sender == 'ortukov97@inbox.ru':
-            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-        elif sender != 'ortukov97@inbox.ru':
-            print (f'Нестандартный отправитель! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-
-send_email('message','stiw.box@mail.ru')
-send_email('message','recipient',)
-
-send_email('mesaage',{'stiw.box@mail.ru'},{'stiw.xod@mail.ru'})
-send_email('message','recipient','sender')
-
-send_email('message',{'ortukov97@inbox.ru'},{'ortukov97@inbox.ru'})
-send_email('message',{'sender'},{'sender'})
-
-
-
+def get_multiplied_digits(numbers):
+    str_numbers = str(numbers)
+    if len(str_numbers) > 1:
+        first = int(str_numbers[0])
+        return first * get_multiplied_digits(int(str_numbers[1:]))
+    else:
+        return int(str_numbers)
+result = get_multiplied_digits(40203)
+print(result)
 
 
